7b6c1e43ee51f242946c691cdc6b8e3b9f50e089
grmdan <grmdan533@gmail.com>
2023-11-20 21:18:59 +0200

    ver 0.1

---
+++ JS_Home_Work/app.js
@@ -0,0 +1,75 @@
+let s1 = "hello";
+
+let s2 = 'owu'
+
+let s3 = 'com'
+
+let s4 = 'ua'
+
+let n1 = 1;
+
+let n2 = 10;
+
+let n3 = -999
+
+let n4 =123;
+
+let n5 = 3.14;
+
+let n6 = 2.7;
+
+let n7 =16;
+
+let b1 = true
+
+let b2 = false
+
+console.log(s1);
+console.log(s2);
+console.log(s3);
+console.log(s4);
+
+console.log(n1);
+console.log(n2);
+console.log(n3);
+console.log(n4);
+console.log(n5);
+console.log(n6);
+console.log(n7);
+
+console.log(b1);
+console.log(b2);
+
+//  TASK 2
+
+let firstName = "Danil "
+
+let middleName = "Dmitrovich"
+
+let lastName = "Babutskiy "
+
+let  person = lastName + firstName + middleName;
+console.log(person)
+
+// task 3
+
+let  a = 100;
+let b = "100";
+let c = true
+
+console.log(typeof  a);
+console.log(typeof  b);
+console.log(typeof  c);
+
+// TASK 4
+
+
+let nameA = prompt("Імям?")
+
+let nameB = prompt("По-Батьков як?")
+
+let ageA = prompt("років скільки?")
+
+console.log("Имья: "  + nameA)
+console.log("По-Батьков: " + nameB)
+console.log("age = " + ageA)
